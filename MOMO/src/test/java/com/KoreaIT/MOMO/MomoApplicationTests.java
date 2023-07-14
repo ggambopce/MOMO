8cee539e01ed99a7c07bf65c841964f9338e31cf
ggambopce <ggambopce@gmail.com>
2023-07-14 14:06:43 +0900

    MOMO세팅

---
+++ MOMO/src/test/java/com/KoreaIT/MOMO/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KoreaIT.MOMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
