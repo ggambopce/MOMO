6990cc3f6d4d563e7a2fd31b92404c671f311554
ggambopce <ggambopce@gmail.com>
2023-07-17 15:57:59 +0900

    generic

--- MOMO/src/main/java/com/KoreaIT/MOMO/controller/UsrArticleController.java
+++ MOMO/src/main/java/com/KoreaIT/MOMO/controller/UsrArticleController.java
@@ -25,7 +25,7 @@ public class UsrArticleController {
 	//액션 메서드 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData doAdd(String title, String body) {
+	public ResultData<Article> doAdd(String title, String body) {
 
 		if (Util.empty(title)) {
 			return ResultData.from("F-1", "제목을 입력해주세요");
@@ -44,12 +44,12 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public ResultData getArticle(int id) {
+	public ResultData<Article> getArticle(int id) {
 
 		Article article = articleService.getArticleById(id);
 
 		if(article == null) {
-			return ResultData.from("F-1", Util.f("%d번 게시물은 존재하지 않습니다.", id));
+			return ResultData.from("F-1", Util.f("%d번 게시물은 존재하지 않습니다", id));
 		}
 		
 		return ResultData.from("S-1", Util.f("%d번 게시물 입니다", id), article);
@@ -57,38 +57,38 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticles() {
+	public ResultData<List<Article>> getArticles() {
 
-		return articleService.getArticles();
+		return ResultData.from("S-1", "게시물 리스트", articleService.getArticles());
 	}
 	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
+	public ResultData doModify(int id, String title, String body) {
 
 		Article article = articleService.getArticleById(id);
 
 		if(article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Util.f("%d번 게시물은 존재하지 않습니다", id));
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return id + "번 게시물을 수정했습니다";
+		return ResultData.from("S-1", Util.f("%d번 게시물을 수정했습니다", id));
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData doDelete(int id) {
 
 		Article article = articleService.getArticleById(id);
 
 		if(article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Util.f("%d번 게시물은 존재하지 않습니다", id));
 		}
 
 		articleService.deleteArticle(id);
 
-		return id + "번 게시물을 삭제했습니다";
+		return ResultData.from("S-1", Util.f("%d번 게시물을 삭제했습니다", id));
 	}
 }
