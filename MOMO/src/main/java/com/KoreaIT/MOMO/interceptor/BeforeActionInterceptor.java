b19db4e0d6c3dbf569203a1ec0ded9a1f01a282f
ggambopce <ggambopce@gmail.com>
2023-07-24 17:05:48 +0900

    interceptor생성

---
+++ MOMO/src/main/java/com/KoreaIT/MOMO/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,19 @@
+package com.KoreaIT.MOMO.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+	
+}
